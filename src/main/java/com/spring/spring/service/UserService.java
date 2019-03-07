c02d48a383600263e30ec468baa0baaad584948f
Nikolay N Gavrilenkov <ngavrilenkov@gmail.com>
2019-03-07 15:53:10 +0300

    Four step

---
+++ src/main/java/com/spring/spring/service/UserService.java
@@ -0,0 +1,27 @@
+package com.spring.spring.service;
+
+import com.spring.spring.repos.IUserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private IUserRepo userRepo;
+
+/////////////// NEW VARIANT //////////////////
+//    private final IUserRepo userRepo;
+//
+//    public UserService(IUserRepo userRepo) {
+//        this.userRepo = userRepo;
+//    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByUsername(username);
+    }
+}
